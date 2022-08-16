734992e96e4237401239f9a93a2ca15fdd40db96
choi <faith-choi@github.com>
2022-08-16 17:35:58 +0900

    상단메뉴바 설정

--- app.js
+++ app.js
@@ -7,13 +7,27 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/static"));
 app.get("/", (req, res) => {
-  console.log("MoodLog");
+  res.render("main");
+});
 
+app.get("/main", (req, res) => {
   res.render("main");
 });
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.get("/rank", (req, res) => {
+  res.render("rank");
+});
+
+app.get("/mypage", (req, res) => {
+  res.render("mypage");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 const port = 3000;
